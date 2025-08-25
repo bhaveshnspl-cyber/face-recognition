0093f1ff264edfb97c6189f9432e5b870abcf428
bhaveshnspl-cyber <bhavesh.nspl@gmail.com>
2025-08-25 17:24:02 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,6 +5,7 @@ const db = mysql.createConnection({
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
+    connectTimeout: 20000,
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0,
